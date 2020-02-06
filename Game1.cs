6dec2f8193d922c9db7fa6c5456f720ba98b7e75
Caio theodoro <caio.theodoro@hotmail.com>
2020-02-06 00:44:25 -0300

    Esqueleto pronto

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Experimentando
 {
@@ -12,10 +13,27 @@ namespace Experimentando
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D target_Sprite;
+        Texture2D crosshairs_Sprite;
+        Texture2D background_Sprite;
+
+        SpriteFont gameFont;
+
+        Vector2 targetPosition = new Vector2(300,300);
+        const int targetRadius = 45;
+        string scoreInfo = "Jogo de teste | Score = {0}";
+        
+
+        MouseState mouseState;
+        bool mRelease = true;
+        float mTargetDist;
+        int score = 0;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            
         }
 
         /// <summary>
@@ -40,6 +58,11 @@ namespace Experimentando
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            target_Sprite = Content.Load<Texture2D>("target");
+            crosshairs_Sprite = Content.Load<Texture2D>("crosshairs");
+            background_Sprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -61,7 +84,25 @@ namespace Experimentando
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            mouseState = Mouse.GetState();
+            mTargetDist = Vector2.Distance(targetPosition, new Vector2(mouseState.X, mouseState.Y));
 
+            if (mouseState.LeftButton == ButtonState.Pressed && mRelease == true)
+            {
+                
+                if(mTargetDist < targetRadius)
+                {
+                    score++;
+                    Random rand = new Random();
+                    targetPosition.X = rand.Next(targetRadius, graphics.PreferredBackBufferWidth - targetRadius) ;
+                    targetPosition.Y = rand.Next(targetRadius, graphics.PreferredBackBufferHeight - targetRadius);
+                 }
+                mRelease = false;
+            } 
+            if(mouseState.LeftButton == ButtonState.Released)
+            {
+                mRelease = true;
+            }
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -74,7 +115,11 @@ namespace Experimentando
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            spriteBatch.Begin();
+            spriteBatch.Draw(background_Sprite,new Vector2( 0,0),Color.White);
+            spriteBatch.Draw(target_Sprite, new Vector2(targetPosition.X - targetRadius,targetPosition.Y - targetRadius), Color.White);
+            spriteBatch.DrawString(gameFont, string.Format(scoreInfo,score.ToString()), new Vector2(0, 0), Color.Black);            
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
